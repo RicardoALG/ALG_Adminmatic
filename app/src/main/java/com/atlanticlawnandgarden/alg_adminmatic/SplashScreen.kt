9d778fbe602e1d63cfd0e2bdc4bda9e162e94d87
RicardoR <ricardo@atlanticlawnandgarden.com>
2018-10-05 16:03:14 -0400

    Image Section

--- app/src/main/java/com/atlanticlawnandgarden/alg_adminmatic/SplashScreen.kt
+++ app/src/main/java/com/atlanticlawnandgarden/alg_adminmatic/SplashScreen.kt
@@ -1,12 +1,19 @@
 package com.atlanticlawnandgarden.alg_adminmatic
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        btn_home.setOnClickListener(({
+            var clickintent = Intent(this@SplashScreen, MainMenu::class.java)
+            startActivity(clickintent)
+        }))
     }
 }
