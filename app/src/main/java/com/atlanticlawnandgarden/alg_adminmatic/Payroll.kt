0b247166c1535f1d070abea0cb2544cd0ec1473f
ricardoALG <ricardo@atlanticlawnandgarden.com>
2018-11-01 13:25:38 -0400

    Payroll OJB function implemented internally.

--- app/src/main/java/com/atlanticlawnandgarden/alg_adminmatic/Payroll.kt
+++ app/src/main/java/com/atlanticlawnandgarden/alg_adminmatic/Payroll.kt
@@ -1,16 +1,13 @@
 package com.atlanticlawnandgarden.alg_adminmatic
 
 import android.app.TimePickerDialog
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_payroll.*
@@ -20,8 +17,11 @@ import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 import android.widget.ArrayAdapter
+import com.android.volley.*
+import com.android.volley.toolbox.StringRequest
 import org.joda.time.DateTimeConstants
 import org.joda.time.LocalDate
+import java.text.DecimalFormat
 
 
 class Payroll : AppCompatActivity() {
@@ -65,8 +65,7 @@ class Payroll : AppCompatActivity() {
 
         getEmployees(urlString)
 
-        //spinnerEmployee.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,employeesList)
-        //spinnerEmployee.setSelection(0,true)
+        GJO(userid, userName,this@Payroll,"https://www.atlanticlawnandgarden.com/cp/app/functions/get/shifts.php",empID,weekStarts,weekEnds)
 
 
 
@@ -179,7 +178,7 @@ class Payroll : AppCompatActivity() {
                 }
 
 
-                GetJsonObj(userid, userName,this@Payroll,"http://www.atlanticlawnandgarden.com/cp/app/functions/get/shifts.php",empID,weekStarts,weekEnds)
+                GJO(userid, userName,this@Payroll,"https://www.atlanticlawnandgarden.com/cp/app/functions/get/shifts.php",empID,weekStarts,weekEnds)
 
                 //getJsonObject(urlString)
 
@@ -266,6 +265,165 @@ class Payroll : AppCompatActivity() {
 
 
         volleyRequest!!.add(employeeRequest)
+    }
+
+    fun GJO(ID: String, user: String,context: Context,Url: String, empID: String, weekStarts: String, weekEnds: String){
+        val params = HashMap<String, String>()
+        params.put("empID", empID)
+        params.put("weekStarts", weekStarts)
+        params.put("weekEnds", weekEnds)
+
+        Log.d("empID:",empID)
+        Log.d("wS:",weekStarts)
+        Log.d("  we: ",weekEnds)
+
+        var sumHours =0.0
+        var shifts =0
+
+        val stringRequest = object : StringRequest(Request.Method.POST, Url, Response.Listener { s ->
+            try {
+
+                val array = JSONObject(s)
+                var shiftsArray = array.getJSONObject("shifts")
+
+                Log.d("Return====",shiftsArray.toString())
+
+
+                val dateTV = intArrayOf(R.id.tit_date_01, R.id.tit_date_02, R.id.tit_date_03, R.id.tit_date_04, R.id.tit_date_05, R.id.tit_date_06, R.id.tit_date_07)
+                val startTV= intArrayOf(R.id.tit_start_01, R.id.tit_start_02, R.id.tit_start_03, R.id.tit_start_04, R.id.tit_start_05, R.id.tit_start_06, R.id.tit_start_07)
+                val stopTV = intArrayOf(R.id.tit_stop_01, R.id.tit_stop_02, R.id.tit_stop_03, R.id.tit_stop_04, R.id.tit_stop_05, R.id.tit_stop_06, R.id.tit_stop_07)
+                val qtyTV = intArrayOf(R.id.tit_qty_01, R.id.tit_qty_02, R.id.tit_qty_03, R.id.tit_qty_04, R.id.tit_qty_05, R.id.tit_qty_06, R.id.tit_qty_07)
+
+                //sunday = date.withDayOfWeek(DateTimeConstants.MONDAY).minusDays(1)
+
+                for(i in 0 until 7){
+
+
+
+                    try{
+                        val start= shiftsArray.getJSONObject(i.toString())
+
+                        var date=start.getString("startTime")
+                        var thisDay=sunday.plusDays(i).toString().substring(5,10)
+
+                        var weekday = sunday.plusDays(i).dayOfWeek().getAsText().substring(0,3)
+                        //weekStarts =thisDay+" ("+weekday+")"
+
+                        var shortDate = weekStarts
+
+
+                        var startHour=start.getString("startTime")
+                        var shortStartHour = startHour.substring(11,16)
+
+                        var endHour=start.getString("endTime")
+                        var shortEndHour = endHour.substring(11,16)
+
+                        var shiftQty=start.getString("shiftQty")
+                        var shortShiftQty = shiftQty
+
+                        sumHours += shortShiftQty.toDouble()
+
+                        if (shortShiftQty!="0.00"){
+
+                            val dt= findViewById(dateTV[i]) as TextView
+                            dt.setText(shortDate)
+
+                            val strt = findViewById(startTV[i]) as TextView
+                            strt.setText(shortStartHour)
+
+                            val stp = findViewById(stopTV[i]) as TextView
+                            stp.setText(shortEndHour)
+
+                            val qty = findViewById(qtyTV[i]) as TextView
+                            qty.setText(shortShiftQty)
+                            shifts++
+                        } else {
+                            val dt = findViewById(dateTV[i]) as TextView
+                            dt.setText(shortDate)
+
+                            val strt = findViewById(startTV[i]) as TextView
+                            strt.setText("-------")
+
+                            val stp = findViewById(stopTV[i]) as TextView
+                            stp.setText("-------")
+
+                            val qty = findViewById(qtyTV[i]) as TextView
+                            qty.setText("-------")
+
+                        }
+
+
+
+
+
+
+
+
+                    }catch(e: JSONException){
+                        var thisDay=sunday.plusDays(i).toString().substring(5,10)
+                        var weekday = sunday.plusDays(i).dayOfWeek().getAsText().substring(0,3)
+                        //weekStarts =thisDay+" ("+weekday+")"
+
+                        Log.d("Error~^^^^",e.toString())
+
+                        var shortDate = weekStarts
+                        val dt =  findViewById(dateTV[i]) as TextView
+                        dt.setText(shortDate)
+
+                        val strt = findViewById(startTV[i]) as TextView
+                        strt.setText("-------")
+
+                        val stp = findViewById(stopTV[i]) as TextView
+                        stp.setText("-------")
+
+                        val qty = findViewById(qtyTV[i]) as TextView
+                        qty.setText("-------")
+
+                        null
+                    }
+
+
+                }
+                val format = DecimalFormat("0.00")
+                val formatted = format.format(sumHours)
+                totalHoursNum.text = "Totals - Shifts:"+shifts+", Hours:"+formatted
+
+
+            }catch (e: JSONException){
+                e.printStackTrace()
+                //Toast.makeText(this,"THERE ARE NO SHIFTS PROGRAMMED FOR NEXT WEEK",Toast.LENGTH_LONG).show()
+                var spinnerPos = Payroll().spinnerEmployee.selectedItemPosition
+                if(spinnerPos==1){
+//                    var toast: Toast = Toast.makeText(this,"THERE ARE NO SHIFTS PROGRAMMED FOR NEXT WEEK", Toast.LENGTH_LONG)
+//                    toast.setGravity(Gravity.CENTER, 0, 0)
+//                    toast.show()
+                    Payroll().spinnerEmployee.setSelection(0)
+                } else {
+//                    var toast: Toast = Toast.makeText(this,"THERE ARE NO SHIFTS PROGRAMMED FOR THIS WEEK", Toast.LENGTH_LONG)
+//                    toast.setGravity(Gravity.CENTER, 0, 0)
+//                    toast.show()
+                    Payroll().spinnerEmployee.setSelection(1)
+                }
+
+
+
+                Log.d("NO DATA RETURNED======","V")
+            }
+        }, Response.ErrorListener {
+            error: VolleyError? ->
+            try {
+                Log.d("ERROR======",error.toString())
+            } catch (e: JSONException){
+                e.printStackTrace()
+
+            }
+        }) {
+            override fun getParams(): Map<String, String> = mapOf("empID" to empID,"startDate" to weekStarts,"endDate" to weekEnds)
+
+        }
+        var appContext: Context? = null
+        val  requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add<String>(stringRequest)
     }
 
 
